23466bda41fd74ad50d93262d6b36072370bbdd4
Vishal Soni <vishal.sony1@gmail.com>
2022-11-28 14:36:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
+
+export default function App() {
+  const [language, setLanguage] = useState("es");
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <Field />
+      <Languages />
+      <hr />
+      <Translate />
+    </div>
+  );
+}
